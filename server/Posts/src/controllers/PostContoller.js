b9b9216bafb781e9a5cdf2c89afdb6405c36b6a2
kiruiaaron <kiruiaaron471@gmail.com>
2023-07-26 11:44:25 +0300

    created endpoints for comments for a specific post, also the enpoint for replies for a specific comment

--- server/Posts/src/controllers/PostContoller.js
+++ server/Posts/src/controllers/PostContoller.js
@@ -46,10 +46,13 @@ async function createPost(req,res){
     const sql = await mssql.connect(config)
     if(sql.connected){
         const{User_id,Written_text,Image_url,Video_url} = req.body
+
+
+
         try {
             const result = await sql.request()
                                     .input('User_id',User_id)
-                                    .input('written_text',Written_text)
+                                    .input('Written_text',Written_text)
                                     .input('Image_url',Image_url)
                                     .input('video_url',Video_url)
                                     .execute('InsertPost')
@@ -242,36 +245,144 @@ async function replyCommentOnPost(req,res){
 }
 
 
-//Like a post
-async function LikePost(req,res){
-    const sql= await mssql.connect(config)
-    if(sql.connected){
-        const {Post_id,User_id}= req.body;
-        try {
-            const result= await sql.request()
-                                  .input('Post_id',Post_id)
-                                  .input('User_id',User_id)
-                                  .execute('LikePost')
+//get comments for a post
 
+async function getComments(req,res){
+    const sql = await mssql.connect(config)
+    if(sql.connected){
+
+        const {Post_id} =  req.params
+        try {
+            const result = await sql.request().input('Post_id',Post_id).execute('GetPostComments')
+            console.log(result.rowsAffected)
+            if(result.rowsAffected === 0){
             res.status(200).json({
-                success:true,
-                message:'You have like this post successfully',
-                result : result.recordset
-            })
+                success:false,
+                message:'No Comments available for this post',
+                result: result.recordset
+            })}else{
+                res.status(200).json({
+                    success:true,
+                    message:'Retrieved all comments for this posts',
+                    result:result.recordset
+                })
+            }
+            console.log(result)
         } catch (error) {
-            console.log(error)
+            console.log(error);
             res.status(404).json({
                 success:false,
-                message:'Post does not exists'
+                message:'Failed to retrieved comments for this  posts'
             })
         }
     }else{
         res.status(500).json({
             success:false,
-            message:'Internal server Error'
+            message:'Internal Server Error'
         })
     }
 }
+
+
+//get replies for comments
+
+async function getCommentsReplies(req,res){
+    const sql = await mssql.connect(config)
+    if(sql.connected){
+
+        const {Comment_id} =  req.params
+        try {
+            const result = await sql.request().input('Comment_id',Comment_id).execute('getCommentReplies')
+            console.log(result.rowsAffected)
+            if(result.rowsAffected === 0){
+            res.status(200).json({
+                success:false,
+                message:'No replies available for this comment',
+                result: result.recordset
+            })}else{
+                res.status(200).json({
+                    success:true,
+                    message:'Retrieved all replies for this comments ',
+                    result:result.recordset
+                })
+            }
+            console.log(result)
+        } catch (error) {
+            console.log(error);
+            res.status(404).json({
+                success:false,
+                message:'Failed to retrieved replies for this  comments '
+            })
+        }
+    }else{
+        res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
+        })
+    }
+}
+
+//like post
+
+async function LikePost(req, res) {
+    const {  User_id} = req.body;
+    const {Post_id}= req.params
+  
+    try {
+      const sql = await mssql.connect(config);
+      if (sql.connected) {
+        // Check if the user has already liked the post
+        const queryResult = await sql
+          .request()
+          
+          .input('Post_id', Post_id)
+          .input('User_id', User_id)
+          .query('SELECT COUNT(*) as count FROM Posts.likeTable WHERE Post_id = @Post_id AND User_id = @User_id');
+  
+        const alreadyLiked = queryResult.recordset[0].count > 0;
+  
+        if (alreadyLiked) {
+          // Unlike the post (delete the like record from the database)
+          await sql
+            .request()
+            .input('Post_id', Post_id)
+            .input('User_id', User_id)
+            .query('DELETE FROM Posts.likeTable WHERE Post_id = @Post_id AND User_id = @User_id');
+  
+          res.status(200).json({
+            success: true,
+            isLiked: false,
+            likeCount: queryResult.recordset[0].count - 1,
+          });
+        } else {
+          // Like the post (add a new like record to the database)
+          await sql
+            .request()
+            .input('Post_id', Post_id)
+            .input('User_id', User_id)
+            .query('INSERT INTO Posts.likeTable (Like_id,Post_id, User_id) VALUES (NEWID(),@Post_id, @User_id)');
+  
+          res.status(200).json({
+            success: true,
+            isLiked: true,
+            likeCount: queryResult.recordset[0].count + 1,
+          });
+        }
+      } else {
+        res.status(500).json({
+          success: false,
+          message: 'Internal server error',
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+      });
+    }
+  }
+  
 
 
 //like a post
@@ -285,5 +396,7 @@ module.exports={
     getUserPost,
     CommentOnPost,
     replyCommentOnPost,
-    LikePost
+    LikePost,
+    getComments,
+    getCommentsReplies
 }
