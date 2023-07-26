e3b721dcd7fa999a8967be8ccb58f57ddf4ef7aa
kiruiaaron <kiruiaaron471@gmail.com>
2023-07-26 11:49:16 +0300

    made little changes that enable me to fetch posts

--- server/Posts/src/server.js
+++ server/Posts/src/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const postRoute = require('./routes/postRoute');
+
 const cors = require('cors')
 require('dotenv').config()
 const app = express();
 
+
+
+
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+    origin:"http://localhost:3001",
+    credentials:true,
+    optionsSuccessStatus:200
+}))
 
 app.get('/',(req,res)=>{
     res.send('welcome to post center');
