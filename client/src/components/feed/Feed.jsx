780bac3d7967371efd8b33bac6849314750c415f
kiruiaaron <kiruiaaron471@gmail.com>
2023-07-26 11:32:28 +0300

    Updated feeds page for mapping post

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./feed.css"
 import Share from '../share/Share'
 import Post from '../post/Post'
-import { dummyPosts } from '../../assets/dummyData/posts'
 import axios from 'axios'
+import { AuthContext } from '../../context/AuthContext'
 
 
-export const Feed = () => {
+export const Feed = ({ UserName}) => {
 
 const [posts,setPosts]= useState([]);
+const {currentUser} = useContext(AuthContext)
 
 useEffect(()=>{
    const fetchPosts = async()=>{
-     const res= await axios.get("http://localhost:8000/posts")
-     const resData= await res.json();
-      console.log(resData.results);
+    try {
+      const res= 
+      UserName? await axios.get(`http://localhost:8000/posts/${UserName}`,{withCredentials:true})
+      :await axios.get(`http://localhost:8000/posts`,{withCredentials:true})
+
+      console.log(res.data.result);
+      setPosts(res.data.result.sort((p1,p2)=>{
+        return new Date(p2.Created_at)- new Date(p1.Created_at);
+      }))
+    } catch (error) {
+       console.log(error)
+    }
+    
    }
    fetchPosts();
-})
+},[UserName])
 
   return (
     <div className='feed'>
         <div className="feedwrapper">
-            <Share/>
-            {dummyPosts.map((post)=>(
-               <Post key={post.id}  post={post}/>
-            ))}
+            {(!UserName || UserName === currentUser?.UserName) &&<Share/>}
+            {
+              posts.map((p)=>(
+                <Post key={p.Post_id} post={p}/>
+              ))
+            }
             
         </div>
     </div>
